3523fd4db49cddd3188d26a1dbb5e24b991ad88f
yulkino <yulkino.chan.desu@gmail.com>
2023-02-25 15:33:19 +0400

    update to .net 7

---
+++ Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(typeof(ServiceCollectionExtensions));
+    }
+}
